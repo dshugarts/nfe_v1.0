4481480a46f66db4594bc1caa65efcf0373d7e3b
Daniel Shugarts <dshugarts@fluidhealthandfitness.com>
2019-10-10 12:05:06 -0500

    delete pages

--- dist/assets/js/pages/dashboard.js
+++ dist/assets/js/pages/dashboard.js
@@ -1,9 +1,3 @@
-/**
- * Theme: Hyper - Responsive Bootstrap 4 Admin Dashboard
- * Author: Coderthemes
- * Module/App: Dashboard
- */
-
 ! function ($) {
     "use strict";
 
@@ -33,23 +27,6 @@
             }
         };
 
-        var draw2 = Chart.controllers.doughnut.prototype.draw;
-        Chart.controllers.doughnut = Chart.controllers.doughnut.extend({
-            draw: function () {
-                draw2.apply(this, arguments);
-                var ctx = this.chart.chart.ctx;
-                var _fill = ctx.fill;
-                ctx.fill = function () {
-                    ctx.save();
-                    ctx.shadowColor = 'rgba(0,0,0,0.03)';
-                    ctx.shadowBlur = 4;
-                    ctx.shadowOffsetX = 0;
-                    ctx.shadowOffsetY = 3;
-                    _fill.apply(this, arguments)
-                    ctx.restore();
-                }
-            }
-        });
 
         var draw3 = Chart.controllers.bar.prototype.draw;
         Chart.controllers.bar = Chart.controllers.bar.extend({
@@ -87,20 +64,8 @@
                 case 'Line':
                     chart = new Chart(ctx, {type: 'line', data: data, options: options});
                     break;
-                case 'Doughnut':
-                    chart = new Chart(ctx, {type: 'doughnut', data: data, options: options});
-                    break;
-                case 'Pie':
-                    chart = new Chart(ctx, {type: 'pie', data: data, options: options});
-                    break;
                 case 'Bar':
                     chart = new Chart(ctx, {type: 'bar', data: data, options: options});
-                    break;
-                case 'Radar':
-                    chart = new Chart(ctx, {type: 'radar', data: data, options: options});
-                    break;
-                case 'PolarArea':
-                    chart = new Chart(ctx, {data: data, type: 'polarArea', options: options});
                     break;
             }
             return chart;
@@ -111,71 +76,11 @@
     // init various charts and returns
     Dashboard.prototype.initCharts = function() {
         var charts = [];
-        
-        if ($('#revenue-chart').length > 0) {
-            var lineChart = {
-                labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-                datasets: [{
-                    label: "Current Week",
-                    backgroundColor: 'transparent', //rgba(114, 124, 245, 0.4)
-                    borderColor: '#727cf5',
-                    data: [32, 42, 42, 62, 52, 75, 62]
-                }, {
-                    label: "Previous Week",
-                    fill: true,
-                    backgroundColor: 'transparent',
-                    borderColor: "#0acf97",
-                    data: [42, 58, 66, 93, 82, 105, 92]
-                }]
-            };
 
-            var lineOpts = {
-                maintainAspectRatio: false,
-                legend: {
-                    display: false
-                },
-                tooltips: {
-                    backgroundColor: '#727cf5',
-                    titleFontColor: '#fff',
-                    bodyFontColor: '#fff',
-                    bodyFontSize: 14,
-                    displayColors: false
-                },
-                hover: {
-                    intersect: true
-                },
-                plugins: {
-                    filler: {
-                        propagate: false
-                    }
-                },
-                scales: {
-                    xAxes: [{
-                        reverse: true,
-                        gridLines: {
-                            color: "rgba(0,0,0,0.05)"
-                        }
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            stepSize: 20
-                        },
-                        display: true,
-                        borderDash: [5, 5],
-                        gridLines: {
-                            color: "rgba(0,0,0,0.01)",
-                            fontColor: '#fff'
-                        }
-                    }]
-                }
-            };
-            charts.push(this.respChart($("#revenue-chart"), 'Line', lineChart, lineOpts));
-        }
-
-        //barchart
-        if ($('#high-performing-product').length > 0) {
+        //dashboard chart
+        if ($('#phase-one-training').length > 0) {
             // create gradient
-            var ctx = document.getElementById('high-performing-product').getContext("2d");
+            var ctx = document.getElementById('phase-one-training').getContext("2d");
             var gradientStroke = ctx.createLinearGradient(0, 500, 0, 150);
             gradientStroke.addColorStop(0, 'rgba(22,134,180,1.0)');
             gradientStroke.addColorStop(1, 'rgba(22,134,180,1.0)');
@@ -228,7 +133,7 @@
                             callback: function(value, index, values) {
                               // for a value (tick) equals to 8
                               return yLabels[value];
-                              // 'junior-dev' will be returned instead and displayed on your chart
+                              // 'error' will be returned instead and displayed on your chart
                         }
                     },
                     }],
@@ -246,90 +151,11 @@
                 },
             };
 
-            charts.push(this.respChart($("#high-performing-product"), 'Bar', barChart, barOpts));
-        }
-
-        if ($('#average-sales').length > 0) {
-            //donut chart
-            var donutChart = {
-                labels: [
-                    "Direct",
-                    "Affilliate",
-                    "Sponsored",
-                    "E-mail"
-                ],
-                datasets: [
-                    {
-                        data: [300, 135, 48, 154],
-                        backgroundColor: [
-                            "#727cf5",
-                            "#fa5c7c",
-                            "#0acf97",
-                            "#ebeff2"
-                        ],
-                        borderColor: "transparent",
-                        borderWidth: "3",
-                    }]
-            };
-            var donutOpts = {
-                maintainAspectRatio: false,
-                cutoutPercentage: 60,
-                legend: {
-                    display: false
-                }
-            };
-            charts.push(this.respChart($("#average-sales"), 'Doughnut', donutChart, donutOpts));
-        }
-        return charts;
-    },
-    // inits the map
-    Dashboard.prototype.initMaps = function() {
-        //various examples
-        if ($('#world-map-markers').length > 0) {
-            $('#world-map-markers').vectorMap({
-                map: 'world_mill_en',
-                normalizeFunction: 'polynomial',
-                hoverOpacity: 0.7,
-                hoverColor: false,
-                regionStyle: {
-                    initial: {
-                        fill: '#e3eaef'
-                    }
-                },
-                markerStyle: {
-                    initial: {
-                        r: 9,
-                        'fill': '#727cf5',
-                        'fill-opacity': 0.9,
-                        'stroke': '#fff',
-                        'stroke-width': 7,
-                        'stroke-opacity': 0.4
-                    },
-
-                    hover: {
-                        'stroke': '#fff',
-                        'fill-opacity': 1,
-                        'stroke-width': 1.5
-                    }
-                },
-                backgroundColor: 'transparent',
-                markers: [{
-                    latLng: [40.71, -74.00],
-                    name: 'New York'
-                }, {
-                    latLng: [37.77, -122.41],
-                    name: 'San Francisco'
-                }, {
-                    latLng: [-33.86, 151.20],
-                    name: 'Sydney'
-                }, {
-                    latLng: [1.3, 103.8],
-                    name: 'Singapore'
-                }],
-                zoomOnScroll: false
-            });
+            charts.push(this.respChart($("#phase-one-training"), 'Bar', barChart, barOpts));
         }
     },
+
+
     //initializing various components and plugins
     Dashboard.prototype.init = function () {
         var $this = this;
